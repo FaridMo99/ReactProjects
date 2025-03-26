c99afc8440a4c9b1bd973984ab9a36016ede0f6b
FaridMo99 <farid-ahmad@hotmail.de>
2025-03-26 23:09:31 +0100

    added everything to CVApplication, only rendering left

--- CVApplication/src/App.jsx
+++ CVApplication/src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
 import Header from "./components/header.jsx";
 import MainSection from "./components/mainSection.jsx";
-import CVResult from "./components/CVResult.jsx";
 
 function App() {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [formData, setFormData] = useState({}); // Store form data for CVResult
-
-  const handleFormSubmit = (data) => {
-    setFormData(data); // Save form data
-    setIsSubmitted(true);
-  };
-
   return (
     <>
       <Header />
-      <MainSection  onSubmit={handleFormSubmit} />
-      {isSubmitted && <CVResult data={formData} />}
+      <MainSection />
     </>
 
   );
--- CVApplication/src/components/CVResult.jsx
+++ CVApplication/src/components/CVResult.jsx
@@ -1,5 +1,6 @@
 function CVResult({ props }) {
-    if(props){
+
+    if(props.length === 10){
 
   return (
     <div className="w-screen h-screen bg-white z-10 overflow-hidden">
@@ -12,10 +13,15 @@ function CVResult({ props }) {
           );
         })}
       </ul>
-      <button type="button" onClick={()=>window.location.reload()}>Reset</button>
+      <button className="bg-teal-950 fixed bottom-6 left-[47.5vw] text-white font-bold w-[5vw] h-[5vh] rounded-md transition-transform transform hover:scale-110 shadow-lg hover:shadow-teal-950 hover:cursor-pointer" type="button" onClick={()=>window.location.reload()}>Reset</button>
     </div>
   )
 }
+
+return null
+
 }
 
 export default CVResult;
+
+//add rendering
